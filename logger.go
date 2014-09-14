306d48b4ea2d925ce27a949e99f837b81ee932fd
Donovan Kolbly <donovan.kolbly@civitaslearning.com>
2014-09-14 14:32:12 -0500

    Added missing warning

--- logger.go
+++ logger.go
@@ -103,6 +103,7 @@ type LevelLogger interface {
 	Debug(format string, args ...interface{})
 	Info(format string, args ...interface{})
 	Notice(format string, args ...interface{})
+	Warning(format string, args ...interface{})
 	Error(format string, args ...interface{})
 	Critical(format string, args ...interface{})
 }
