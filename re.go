9ad6e1ee34daecf4812bb3883ea2d68fc3bd0532
Donovan Kolbly <donovan@rscheme.org>
2015-04-11 08:10:01 -0500

    Allow log contextualization Re() to stack

--- re.go
+++ re.go
@@ -1,13 +1,29 @@
 package logging
 
+type Annotater interface {
+	Annotate(*Record)
+}
+
+// an annotator stack level
+type stacked struct {
+	lower, higher Annotater
+}
+
+func (s stacked) Annotate(rec *Record) {
+	s.lower.Annotate(rec)
+	s.higher.Annotate(rec)
+}
+
 // Re returns a new logger that annotates log messages with the
 // appropriate context (as defined by the Annotater)
 func (l *Logger) Re(a Annotater) *Logger {
+
+	// if there's an existing annotator, stack them together
+	if l.annotater != nil {
+		a = stacked{l.annotater, a}
+	}
+
 	lcopy := *l
 	lcopy.annotater = a
 	return &lcopy
-}
-
-type Annotater interface {
-	Annotate(*Record)
 }
